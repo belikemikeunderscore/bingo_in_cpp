1cdfc91a169ef6345e7acbd898771f1913478f82
mike <miguel.bras06@gmail.com>
2024-01-24 12:28:14 -0100

    it brokey

--- main.cpp
+++ main.cpp
@@ -1,130 +1,117 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cstdlib>
-#include <ctime>
-#include <numeric>
-#include <limits>
+#include <unistd.h>
 #include "cardgen.h"
+
+int NUMEROS;
+int choice;
+#define COLUNAS     10
+#define DELAY       1000000 //1 segundo
+#define MODO_AUTO   false
+
 using namespace std;
 
-int selection;
-int amountDeNumerosSort;
-int poolselect;
-int sorteados;
-int numeroSorteado;
-int anterior;
-int contador;
-
-void displayNumbers(const vector<int>& sortedNumbers) {
-
-    int totalNumbers = sortedNumbers.size();
-
-    int rows = totalNumbers / 5 + (totalNumbers % 5 != 0);
-
-    int cols = 5;
 
 
-    for (int row = 0; row < rows; ++row) {
-
-        for (int col = 0; col < cols; ++col) {
-
-            int index = row * cols + col;
-
-            if (index < totalNumbers) {
-
-                cout << setw(2) << sortedNumbers[index] << ' ';
-
-            } else {
-
-                cout << " ";
-
-            }
-
-        }
-
-        cout << endl;
-
-    }
-
+int geraNumero(){
+    return rand() % NUMEROS + 1;
 }
+
+
+void geraChave(int chave[]){
+    int i = 0, j = 0, aux = 0;
+    bool existent = false;
+    while(i < NUMEROS){
+        aux = geraNumero();
+        for(j = 0; j < i; j++){
+            if(aux == chave[j]){
+                existent = true;
+                break;
+            }
+        }
+        if(!existent){
+            chave[i] = aux;
+            i++;
+        }
+        existent = false;
+    }
+}
+
+
+void ordenaChave(int chave[]){
+    int aux = 0;
+    for(int i = 0; i < NUMEROS; i++){
+        for(int j = i + 1; j < NUMEROS; j++){
+            if(chave[i] > chave[j]){
+                aux = chave[i];
+                chave[i] = chave[j];
+                chave[j] = aux;
+            }
+        }
+    }
+}
+
 
 int main(){
-    srand(static_cast<unsigned>(time(0)));
-    system("clear");
-    cout << "Bem vindo ao bingo!\n";
-    cout << "1) Jogar\n";
-    cout << "2) Gerar Cartões\n";
-    cin >> selection;
-    system("clear");
-        switch (selection){
-            case 1:
-                    int totalNumbers;
-    cout << "Escolha o tipo de sorteio:\n1) 75 números\n2) 90 números\n3) 100 números\n";
-    int choice;
-    cin >> choice;
 
-    switch (choice) {
+
+    int resposta;
+    int ctrl = 0;
+
+    do{
+        system("clear");
+        cout << "Bem vindo ao bingo!" << endl;
+        cout << "1) Jogar\n2) Gerar cartões\n3) Sair\n\n";
+
+       cin >> resposta; 
+
+       switch(resposta){
+       case 3:
+           break;
+        case 2:
+            gen();
         case 1:
-            totalNumbers = 75;
+            do{
+        
+        system("clear");
+        int chave[NUMEROS];
+        geraChave(chave);
+        cout << "Quantos numeros queres sortear?\n1)75\n2)90\n3)100\n";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            NUMEROS = 75;
             break;
         case 2:
-            totalNumbers = 90;
+            NUMEROS = 90;
             break;
         case 3:
-            totalNumbers = 100;
+            NUMEROS = 100;
             break;
-        default:
-            cout << "Escolha inválida!\n";
-            return 1;
     }
-
-    vector<int> allNumbers(totalNumbers);
-    iota(allNumbers.begin(), allNumbers.end(), 1);
-
-    vector<int> sortedNumbers;
-    int previousNumber = -1;
-
-    int drawMode;
-    cout << "Escolha o modo de sorteio:\n1) Automático\n2) Manual\n";
-    cin >> drawMode;
-
-    while (true) {
-        if (drawMode == 1) {
-            random_shuffle(allNumbers.begin(), allNumbers.end());
-            int drawnNumber = allNumbers.back();
-            allNumbers.pop_back();
-
-            sortedNumbers.push_back(drawnNumber);
-            cout << "Número sorteado: " << drawnNumber << "\nNúmero anterior: " << previousNumber << endl;
-        } else if (drawMode == 2) {
-            char manualDraw;
-            cout << "Pressione qualquer tecla para sortear um novo número (ou 'q' para sair): ";
-            cin >> manualDraw;
-
-            if (manualDraw == 'q' || manualDraw == 'Q') {
-                break;
-            }
-
-            int drawnNumber = allNumbers.back();
-            allNumbers.pop_back();
-
-            sortedNumbers.push_back(drawnNumber);
-            cout << "Número sorteado: " << drawnNumber << "\nNúmero anterior: " << previousNumber << endl;
-        } else {
-            cout << "Modo de sorteio inválido!\n";
-            return 1;
-        }
-
-        previousNumber = sortedNumbers.back();
-        displayNumbers(sortedNumbers);
-    }
-                cout << "Penis 2";
-                break;
+        cout << "Números Sorteados ==============================================" << endl;
+        for(int i = 0; i < NUMEROS; i++){
             
+            cout << chave[i];
+            usleep(DELAY);
+            cout << "\t" << flush; 
+            if((i + 1) % COLUNAS == 0){
+                cout << endl;
+            }
+        }
+        cout << endl;
+        cout << "Todos os numeros foram sorteados.\nJogar novamente? (s/n) ";
+        cin >> resposta;
+    }while(resposta == 's');
+    
+    return 0;
 
-
-        } 
-    gen();
-
-}
+       default:
+           cout << "Opção inválida" << endl;
+           usleep(DELAY);
+       }
+    }while(true);
+    
+    
+    }
+      
